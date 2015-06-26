fa96a06061bb10cb06ee5425122b91b081296a67
Chaitanya Varma <chaitany@thoughtworks.com>
2015-06-26 11:39:43 +0530

    Refactored code for calculating distance between two points

--- src/com/twu/pathasala/Point.java
+++ src/com/twu/pathasala/Point.java
@@ -8,13 +8,6 @@ public class Point {
     }
 
     public double distanceFrom(Point otherPoint) {
-        if(coordinateY == otherPoint.coordinateY)
-            return Math.abs(coordinateX - otherPoint.coordinateX);
-        else if(coordinateX == otherPoint.coordinateX)
-            return Math.abs(coordinateY - otherPoint.coordinateY);
-        else if(coordinateY ==0 && otherPoint.coordinateX ==0)
-            return Math.sqrt(Math.pow(coordinateX, 2)+ Math.pow(otherPoint.coordinateY, 2));
-        else
             return Math.sqrt(Math.pow(coordinateX - otherPoint.coordinateX, 2)+ Math.pow(coordinateY-otherPoint.coordinateY, 2));
     }
 }
