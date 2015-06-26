e63775c7a8d1e7367e91cce2d781520edaf89cc4
Chaitanya Varma <chaitany@thoughtworks.com>
2015-06-26 16:32:55 +0530

    Refactored code for calculating area

--- src/com/twu/pathasala/Quadrilateral.java
+++ src/com/twu/pathasala/Quadrilateral.java
@@ -11,19 +11,6 @@ public class Quadrilateral {
     }
 
     public double area() {
-        if((firstPoint.coordinateX == secondPoint.coordinateX && secondPoint.coordinateX ==thirdPoint.coordinateX && thirdPoint.coordinateX == fourthPoint.coordinateX)
-                ||(firstPoint.coordinateY == secondPoint.coordinateY && secondPoint.coordinateY ==thirdPoint.coordinateY && thirdPoint.coordinateY == fourthPoint.coordinateY))
-        return 0;
-        else if(firstPoint.distanceFrom(secondPoint) == secondPoint.distanceFrom(thirdPoint)
-                && secondPoint.distanceFrom(thirdPoint)== thirdPoint.distanceFrom(fourthPoint)
-                && thirdPoint.distanceFrom(fourthPoint)== fourthPoint.distanceFrom(firstPoint))
-            return Math.pow(firstPoint.distanceFrom(secondPoint),2);
-        else if(firstPoint.distanceFrom(secondPoint)== thirdPoint.distanceFrom(fourthPoint)
-                && secondPoint.distanceFrom(thirdPoint)== firstPoint.distanceFrom(fourthPoint)
-                && firstPoint.distanceFrom(secondPoint)== secondPoint.distanceFrom(thirdPoint)
-                && secondPoint.distanceFrom(thirdPoint)== thirdPoint.distanceFrom(fourthPoint))
-            return firstPoint.distanceFrom(secondPoint)*secondPoint.distanceFrom(thirdPoint);
-        else
             return Math.abs(firstPoint.coordinateX*secondPoint.coordinateY - secondPoint.coordinateX*firstPoint.coordinateY
                 + secondPoint.coordinateX*thirdPoint.coordinateY - thirdPoint.coordinateX*secondPoint.coordinateY
                 + thirdPoint.coordinateX*fourthPoint.coordinateY - fourthPoint.coordinateX*thirdPoint.coordinateY
